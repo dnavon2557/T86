f19cd9fddcb485cd2969b4011af5991bd467d52a
Daniel Navon <dnavon2557@gmail.com>
2016-02-02 15:01:05 -0500

    removed comments around form loading

--- load_forms.js
+++ load_forms.js
@@ -1,9 +1,9 @@
-/*$(document).ready( function () {
+$(document).ready( function () {
 	var password = prompt("Enter the password");
 	$.post('getForms', {pass:password}, function (data) {
 		$('#forms').html(data);
    	});
-});*/
+});
 $('#addMission').ready( function () {
 	$.get('getMission', function (data) {
 		$('#mission_statement').val(data);
